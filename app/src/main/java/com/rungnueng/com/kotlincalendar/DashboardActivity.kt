925263da7ed86f71247b53af08424038c5bb9344
RUNGNUENG <rungnueng19@hotmail.com>
2019-01-03 10:52:45 +0700

    add toolbar

--- app/src/main/java/com/rungnueng/com/kotlincalendar/DashboardActivity.kt
+++ app/src/main/java/com/rungnueng/com/kotlincalendar/DashboardActivity.kt
@@ -1,12 +1,42 @@
 package com.rungnueng.com.kotlincalendar
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        initInstance(savedInstanceState)
+    }
+
+    private fun initInstance(savedInstanceState: Bundle?) {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(this,"a",Toast.LENGTH_LONG).show()
+//                val intent = Intent(this,ShopActivity::class.java)
+//                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
